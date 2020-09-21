6d5662322d78de6dff37054943c9be8447545de2
bmuhamood <bbosa2009@gmail.com>
2020-09-21 21:45:00 +0400

    fixing linters

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../lib/logic.rb'
 
 describe Logic do
-  let(:game_draw) { Logic.new(%w[X O X X O X O X O])}
-  let(:game_incomplete) {Logic.new(['X', 'O', 'X', 'X', 5, 6, 7, 8, 9])}
+  let(:game_draw) { Logic.new(%w[X O X X O X O X O]) }
+  let(:game_incomplete) { Logic.new(['X', 'O', 'X', 'X', 5, 6, 7, 8, 9]) }
 
   describe '#draw?' do
     it 'returns nil when the game ends in a draw' do
